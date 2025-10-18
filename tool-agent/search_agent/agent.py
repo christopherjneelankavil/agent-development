b1fffb17e5575cbae05b41f5cb680296e8b82933
christopherjneelankavil <christopherneelankavil@gmail.com>
2025-10-18 18:48:55 +0530

    Added tool based agent
    feat : -> added a tool based agent for google search and for finding current date and time
    feat : -> imported agent in __init__.py file
    
    Updated files
    refactor : -> updated gitignore to exclude .env file in search_agent directory
    refactor : -> updated requirements.txt to include and remove some dependencies

---
+++ tool-agent/search_agent/agent.py
@@ -0,0 +1,34 @@
+# import necessary modules
+from datetime import datetime
+from google.adk.agents import Agent
+from google.adk.tools import google_search
+
+
+# function to get the current date in YYYY-MM-DD format
+def get_current_date_and_time() -> dict:
+    """
+    Get the current date and time in YYYY-MM-DD format.
+    :return: {"date": "YYYY-MM-DD"}
+    """
+    return {
+        "current_date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    }
+
+
+root_agent = Agent(
+    name="search_agent",
+
+    # specify the model
+    model="gemini-2.0-flash",
+    description="A search tool agent to go and fetch data",
+
+    #specify the instruction to be sent
+    instruction="""
+    You are a helpful and friendly assistant that can use the following tool:
+    - google_search
+    """,
+
+    # specify the tool needed to be used here
+    tools=[google_search],
+    # tools=[get_current_date_and_time]
+)
